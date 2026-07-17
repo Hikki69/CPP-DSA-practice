f5692dfa42bcaeeaa9c58059297bafb6039de6f7
Harsh_Jha <mharshjha9@gmail.com>
2026-07-17 20:47:48 +0530

    .

---
+++ linked_list/10_PalindromeLL/brute.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node*next;
+    
+
+    public:
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+      
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+       
+    }
+};
+
+Node* convertArr2LL(vector<int> &arr){
+    if(arr.empty()) return nullptr;
+
+    Node* head = new Node(arr[0],nullptr);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i],nullptr);
+        mover->next = temp;
+        mover = temp;
+    }
+    
+    return head;
+}
+
+bool palindromeLL(Node*head){
+    Node* temp = head;
+    stack<int>st;
+    while(temp!=NULL){
+        st.push(temp->data);
+        temp = temp->next;
+    }
+    temp = head;
+    while(temp!=NULL){
+        if(temp->data != st.top())return false;
+        temp = temp->next;
+        st.pop();
+    }
+    return true;
+}
+
+void print(Node*head){
+    while(head != NULL){
+        cout << head->data<<" ";
+        head = head->next;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    Node* head = convertArr2LL(a);
+    
+    
+    cout<<palindromeLL(head);
+    return 0;
+}
+
+// time complexity 
+// TC = O(2n)
+
+// space complexity
+// SC = O(n)
